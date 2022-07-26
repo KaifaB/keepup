ac4d923cea8a20b73a8ed5b7d87ca30dfeb85fa2
Kaifa Best <Best.Kai93@gmail.com>
2022-07-26 12:52:32 -0700

    almost finished html of log in page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
+import React from 'react'
+import { useState } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+
+function Login() {
+  const [tab, setTab] = useState(1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="login-container">
+      <div className="contain-both">
+        <div className="choices">
+          <div className={(tab === 1) ? 'choice active' : 'choice'} onClick={() => setTab(1)} for="login"><h1>Login</h1></div>
+          <div className={(tab === 2) ? 'choice active' : 'choice'} onClick={() => setTab(2)} for="signup"><h1>Sign Up</h1></div>
+        </div>
+        <div className={(tab === 1) ? 'login show' : 'login'}>
+          <span className='flex line'><h2>Username:</h2><input type="text"></input></span>
+          <span className='flex line'><h2>Password:</h2><input type="text"></input></span>
+          <span className='flex line'>
+          <button>Log In</button>
+          <button>Log In as Guest</button>
+          </span>
+        </div>
+        <div className={(tab === 2) ? 'signup show' : 'signup'}>
+          <span className='flex line'><h2>Username:</h2><input type="text"></input></span>
+          <span className='flex line'><h2>Email:</h2><input type="text"></input></span>
+          <span className='flex line'><h2>Password:</h2><input type="text"></input></span>
+          <span className='flex line'><h2>Confirm Password:</h2><input type="text"></input></span>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Login;
